8a2cf990522be82eb27df07895346e6dea3c3eb0
dustinpfister <dustin.pfister@gmail.com>
2018-01-09 23:12:48 -0500

    box-spin demo changed for new system

--- source/_forframe/box-spin/index.js
+++ source/_forframe/box-spin/index.js
@@ -1,29 +1,53 @@
-forFrame.add({
+forFrame({
 
-    name: 'hello-world',
-
+    name: 'box-spin',
     init: function (api) {
 
-        // and a single box with the id 'theBox';
-        api.addBox.call(this, 'theBox');
+        this.add('graphics', 'item', function () {
+
+            this.beginFill(0xff0000);
+            this.drawRect(-50, -50, 100, 100);
+            this.endFill();
+
+        });
 
     },
 
-    forFrame: function (api) {
+    forFrame: function () {
 
-        var bx = api.get.call(this, 'theBox');
-
-        bx.w = 32;
-        bx.h = 32;
-
-        let cx = this.canvas.width / 2 - bx.w / 2;
-        let cy = this.canvas.height / 2 - bx.h / 2;
-
-        let r = Math.PI * 2 * this.per;
-
-        bx.x = cx + Math.cos(r) * 100;
-        bx.y = cy + Math.sin(r) * 100;
+        var item = this.get('item');
 
     }
 
 });
+/*
+forFrame.add({
+
+name: 'box spin',
+
+init: function (api) {
+
+// and a single box with the id 'theBox';
+api.addBox.call(this, 'theBox');
+
+},
+
+forFrame: function (api) {
+
+var bx = api.get.call(this, 'theBox');
+
+bx.w = 32;
+bx.h = 32;
+
+let cx = this.canvas.width / 2 - bx.w / 2;
+let cy = this.canvas.height / 2 - bx.h / 2;
+
+let r = Math.PI * 2 * this.per;
+
+bx.x = cx + Math.cos(r) * 100;
+bx.y = cy + Math.sin(r) * 100;
+
+}
+
+});
+*/
