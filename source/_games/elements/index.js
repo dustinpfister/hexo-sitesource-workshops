5fb421d732c33b07c429ea18da17499816873db7
dustinpfister <dustin.pfister@gmail.com>
2018-01-20 14:06:25 -0500

    added new files that make used of my new plugin hexo-game-studio

---
+++ source/_games/elements/index.js
@@ -0,0 +1,14 @@
+
+
+var game = new Phaser.Game(320, 240, Phaser.auto, 'game-area', {
+
+        create: function () {
+
+            console.log('okay');
+
+        },
+
+        update: function () {}
+
+    });
+
