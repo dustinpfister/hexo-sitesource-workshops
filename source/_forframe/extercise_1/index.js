06d7d7dd14eb21021223b683d9c08c27d5f4d660
dustinpfister <dustin.pfister@gmail.com>
2018-01-21 17:37:30 -0500

    the idea I had is working

--- source/_forframe/extercise_1/index.js
+++ source/_forframe/extercise_1/index.js
@@ -1,28 +1,38 @@
 forFrame({
 
     name: 'hello-world',
-    maxFrame: 500,
+    maxFrame: 200,
 
     init: function (api) {
 
         var game = this.game;
 
+        var self = this;
         var i = 0;
         while (i < 4) {
 
-            this.add('graphics', 'bx' + (i + 1), function (ff) {
+            (function () {
 
-                this.data.h = 20
-                    this.data.w = 20;
+                var val = Math.random() * 255;
 
-                this.clear();
-                this.beginFill(0x00ff00);
-                this.drawRect(0, 0, this.data.w, this.data.h);
-                this.endFill();
+                self.add('graphics', 'bx' + (i + 1), function (ff) {
 
-            });
+                    this.data.w = 20 + 110 * ff.bias;
+                    this.data.h = 20 + 20 * ff.bias;
+                    //this.data.h = 20;
 
-            i += 1;
+                    this.clear();
+                    this.beginFill(parseInt(Math.floor(val).toString(16) + '00', 16));
+                    this.drawRect(0, 0, this.data.w, this.data.h);
+                    this.endFill();
+
+                });
+
+                i += 1;
+
+            }
+                ());
+
         }
 
     },
@@ -31,11 +41,24 @@ forFrame({
 
         var bx,
         game = this.game,
-        i = 0;
+        i = 0,
+
+        // deltas for each box
+        dx = [110, 90, 110, 90],
+        dy = [-75, -25, 55, 5];
         while (i < 4) {
 
-            bx = this.get('bx' + (bx + 1)).disp;
-            bx.x = 10 + 20 * i;
+            bx = this.get('bx' + (i + 1)).disp;
+
+            // grid x, and grid y
+            var gx = i % 2,
+            gy = Math.floor(i / 2),
+
+            sx = 10 + gx * 20,
+            sy = 10 + gy * 20 + 100;
+
+            bx.x = sx + dx[i] * this.bias;
+            bx.y = sy + dy[i] * this.bias;
 
             i += 1;
 
